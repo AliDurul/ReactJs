ab8a0f497f08dd710d8072de3d67edbbbf17f32b
Ali <alidrl26@gmail.com>
2023-08-07 18:05:42 +0200

    COUNTER DONE  WITH REDUCER

--- redux-intro-starter/src/components/counter/Counter.jsx
+++ redux-intro-starter/src/components/counter/Counter.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux"
 import "./Counter.css"
+import {  clear, decrement, increment } from "../../store/counterReducer"
 
 const Counter = () => {
 
@@ -12,9 +13,9 @@ const Counter = () => {
       <h1>counter:{count}</h1>
       <div>
         <button className="counter-button positive"
-          onClick={() => dispatch({ type: "INC" })}>increase</button>
-        <button onClick={() => dispatch({ type: "CLR" })} className="counter-button zero">reset</button>
-        <button  onClick={() => dispatch({ type: "DEC" })} className="counter-button negative">decrease</button>
+          onClick={() => dispatch(increment())}>increase</button>
+        <button onClick={() => dispatch(clear())} className="counter-button zero">reset</button>
+        <button  onClick={() => dispatch(decrement())} className="counter-button negative">decrease</button>
       </div>
     </div>
   )
--- redux-intro-starter/src/store/counterReducer.js
+++ redux-intro-starter/src/store/counterReducer.js
@@ -1,14 +1,22 @@
+ const INC = "INC";
+ const DEC = "DEC";
+ const CLR = "CLR";
+
+export const increment = () => ({ type: INC });
+export const decrement = () => ({ type: DEC });
+export const clear = () => ({ type: CLR });
+
 const initialState = {
   count: 0,
 };
 
 export const counterReducer = (state = initialState, { type }) => {
   switch (type) {
-    case "INC":
+    case INC:
       return { count: state.count + 1 };
-    case "DEC":
+    case DEC:
       return { count: state.count - 1 };
-    case "CLR":
+    case CLR:
       return { count: 0 };
 
     default:
